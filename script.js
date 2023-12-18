ad9b42ee309380cd8b9eb4b68fc23063e2e3909d
karthi5555 <105117281+karthi5555@users.noreply.github.com>
2023-12-18 11:31:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function mapLetters(str){
+    let arr = {};
+    for(let i=0; i<str.length; i++){
+        if(arr[str[i]]){
+            arr[str[i]].push(i);
+        }
+        else{
+            arr[str[i]] = [ i ];
+        }
+    }
+    return arr;
+}
+let str = prompt("enter a string");
+mapLetters(str);
